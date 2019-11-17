d7916efd1aac90e1d3b36bf674c3714913dd674d
Yalcin TOK <yalcintok33@gmail.com>
2019-11-17 11:24:56 +0100

    Test Done

--- version1/test.js
+++ version1/test.js
@@ -10,6 +10,22 @@
 // Hint: use the modulo operator (%) to figure out if it's a multiple of 3/5. Make sure the remainder is 0, in order to pass the condition.
 // Hint: the order of conditional statements is important!
 
+function getNumbers () {
+    for (let i = 0; i <= 100; i++) {
+        if(i % 15 == 0) {
+            console.log('Jackpot!')
+        } else if (i % 3 == 0) {
+            console.log('This is a multiple of 3')
+        } else if (i % 5 == 0) {
+            console.log('This is a multiple of 5')
+        } else {
+            console.log(i);
+        }
+    }
+}
+getNumbers();
+
+
 // 2.
 // Using JavaScript only (adding HTML to index.html is NOT allowed), write a function that:
 // - Creates a button element (with text "click me!")
@@ -18,8 +34,44 @@
 // - Removes the button after the click
 // - Use the following image URL: https://thehub.dk/files/5ad4b4a9f9ac4aa13c3d2d58/logo_upload-6d537cf7e5de664db275b32b3c6ae12d.png
 
+
+// function getImage () {
+//     let button = document.createElement('button');
+//     button.textContent = 'Click Me!';
+//     document.body.appendChild(button);
+//     button.addEventListener('click', () => {
+//         let image = document.createElement('img')
+//         // Because of the url which was given us did not work I used another url which I found in google
+//         image.setAttribute('src', 'https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+//         document.body.appendChild(image);
+//         button.remove();
+//     })
+// }
+// getImage();
+
+
 // 3.
 // Write a function that:
 // - Makes an API call using the Fetch API or the regular XMLHttpRequest
 // - Use the following API: https://randomuser.me/api?results=3
 // - Parse the response and then display the "first" and "last" names of the first three users within the DOM (inside an unordered list)
+
+
+// const ApiURL = 'https://randomuser.me/api?results=3';
+// function fetchData (url) {
+//     fetch(url)
+//         .then(response => response.json().then(data => {
+//             let userArray = data.results;
+//             let output = '';
+//             userArray.forEach(user => {
+//                 output += `<ul>
+//                 <li><strong>Name:</strong> ${user.name.first}</li>
+//                 <li><strong>Surname:</strong> ${user.name.last}</li>    
+//                 </ul>`
+//             });
+//             document.body.innerHTML = output;
+//         }))
+//         .catch(err => console.log('Something went wrong', err))
+// }
+
+// fetchData(ApiURL);
